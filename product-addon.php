edd3f3788d0814d96cb23590fe2397231db45932
Sheeba Abraham <sheeba@yapapaya.com>
2016-10-25 10:43:16 +0530

    Initial commit

---
+++ product-addon.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The plugin main file
+ *
+ *
+ * @link              isheeba.com
+ * @since             1.0.0
+ * @package           Product_Addon
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Product Addon
+ * Plugin URI:        isheeba.com
+ * Description:       A plugin to add product content type with price, description, image etc.
+ * Version:           1.0.0
+ * Author:            Sheeba
+ * Author URI:        iSheeba.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       product-addon
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+
